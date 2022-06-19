92de84b89bbde033b3625299a05a3c800e589348
Facundo <ff96_@hotmail.com>
2022-06-19 12:44:49 -0300

    continuando el 21

--- JavaIntroEj21/src/javaintroej21/JavaIntroEj21.java
+++ JavaIntroEj21/src/javaintroej21/JavaIntroEj21.java
@@ -8,6 +8,8 @@ la matriz M en la cual empieza el primer elemento de la submatriz P.
  */
 package javaintroej21;
 
+import java.util.Arrays;
+
 /**
  *
  * @author Facundo
@@ -18,7 +20,59 @@ public class JavaIntroEj21 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int[][] matrizM = new int[10][10];
+        int[][] matrizP = new int[3][3];
+
     }
-    
+
+    public static void dibujarMatriz(int[][] matriz) {
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                matriz[i][j] = (int) (Math.random() * 10);
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println("");
+        }
+    }
+
+    public static void isInside(int[][] matrizA, int[][] matrizB) {
+        boolean inside = false;
+        int count = 0;
+        int count2 = 0;
+        int a = 0;
+        int b = 0;
+        int[] arr = new int[9];
+        int[] arr2 = new int[9];
+
+        while (count <= 9 && count2 < 100) {
+
+            for (int i = 0; i < matrizA.length; i++) {
+                for (int j = 0; j < matrizA.length; j++) {
+                    a = matrizA[i][j];
+                }
+            }
+
+            for (int i = 0; i < matrizB.length; i++) {
+                for (int j = 0; j < matrizB.length; j++) {
+                    b = matrizB[i][j];
+                    count2 += 1;
+                    if (a == b) {
+                        count += 1;
+                        arr[i] = i;
+                        arr2[j] = j;
+                    }
+                }
+            }
+            
+            if (count == 9) {
+                String toString1 = Arrays.toString(arr);
+                String toString2 = Arrays.toString(arr2);
+                System.out.println("La primera matriz se encuentra contenida en la segunda matriz en las posiciones: Y: " + toString1 + " X: " + toString2);
+            } else {
+                System.out.println("No se encuentra la primera matriz dentro de la segunda matriz");
+            }
+
+        }
+    }
 }
